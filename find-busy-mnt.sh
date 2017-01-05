d04dffb5ece26f3dedf8c8cc2398965d5c86819f
Vivek Goyal <vgoyal@redhat.com>
2017-01-05 14:15:46 -0500

    Initial version of find-busy-mnt.sh
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>

---
+++ find-busy-mnt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# A simple script to get information about mount points and pids and their
+# mount namespaces.
+
+if [ $# -ne 1 ];then
+  echo "Usage: $0 <devicemapper-device-id>"
+  exit 1
+fi
+
+ID=$1
+
+MOUNTS=`find /proc/*/mounts | xargs grep $ID`
+
+#echo "$MOUNTS"
+
+printf "PID\tNAME\t\tMNTNS\n"
+echo "$MOUNTS" | while read LINE; do
+  PID=`echo $LINE | cut -d ":" -f1 | cut -d "/" -f3`
+  NAME=`ps -q $PID -o comm=`
+  MNTNS=`readlink /proc/$PID/ns/mnt`
+  printf "%s\t%s\t\t%s\n" "$PID" "$NAME" "$MNTNS"
+done
