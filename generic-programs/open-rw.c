24e89ff4650a3778749f47bd785e02d9a0a7614e
Vivek Goyal <vgoyal@redhat.com>
2018-06-18 08:48:41 -0400

    Add open-rw.c
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>

---
+++ generic-programs/open-rw.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int fd, flags = 0;
+	mode_t mode = 0;
+
+	if (argc != 2) {
+		printf("Usage:%s <file-to-open>\n", argv[0]);
+		exit(1);
+	}
+
+	fd = open(argv[1], O_RDWR);
+	if (fd == -1)
+		fprintf(stderr, "Failed to open file %s:%s, errorno=%d\n", argv[0], strerror(errno), errno);
+
+	close(fd);
+}
